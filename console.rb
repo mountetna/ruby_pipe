ce44c7b71706f1e8b0069ad4187af7d5493efa3c
Saurabh Asthana <rednblack@alum.mit.edu>
2013-08-29 12:53:15 -0700

    some utilities

--- console.rb
+++ console.rb
@@ -4,21 +4,14 @@ require 'pry'
 require 'yaml'
 
 ENV['LIB_DIR'] = File.dirname File.realdirpath(__FILE__)
-puts ENV['LIB_DIR']
 ENV['CONFIG'] = ARGV[0]
-
 __config__ = YAML.load_file ENV['CONFIG']
-
 $: << ENV['LIB_DIR'] + "/lib"
-
-# make some dummy variables
-
 require __config__[:pipe].downcase + '/' + __config__[:script]
+
 # Create the pipe
 pipe = Kernel.const_get( __config__[:pipe].camel_case ).const_get( __config__[:script].camel_case).new
-
 
 # do what needs to be done
 ARGV.clear
 binding.pry
-
