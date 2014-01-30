a586e91124e20198241fc60b52cea58e85426b9f
Saurabh Asthana <rednblack@alum.mit.edu>
2014-01-30 12:17:14 -0800

    allowing local-copy symlinks of pipeline for easier testing

--- run_pipe.rb
+++ run_pipe.rb
@@ -7,6 +7,8 @@ $: << ENV['LIB_DIR'] + "/lib"
 
 pipe,script = File.basename(__FILE__).split(/\_/,2)
 
+pipe.sub! /^local./,""
+
 require pipe + '/' + script
 
 s = Kernel.const_get( pipe.camel_case).const_get( script.camel_case).new
