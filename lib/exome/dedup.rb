49e53e2ef7f7bd703b027a10dc31ffcf24aa6391
Saurabh Asthana <rednblack@alum.mit.edu>
2014-04-03 10:37:35 -0700

    moving deduplication to after consolidation, stupid stupid rat-creatures

---
+++ lib/exome/dedup.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+module Exome
+  class Dedup
+    include Pipeline::Step
+    runs_tasks :merge_sam_files
+    runs_on :samples
+    resources :threads => 12
+
+    class MarkDuplicates
+      include Pipeline::Task
+      requires_file :aligned_bams
+      outs_file :raw_sample_bam, :duplication_metrics
+
+      def run
+	log_info "Mark duplicates"
+	picard :mark_duplicates, :INPUT => config.aligned_bams,
+          :OUTPUT => config.raw_sample_bam,
+          :METRICS_FILE => config.duplication_metrics, 
+          :CREATE_INDEX => :true,
+          :REMOVE_DUPLICATES => :false or error_exit "Mark duplicates failed"
+      end
+    end
+  end
+end
